2cef65ad7242604d0c9d29a2974c627b1081dded
Bima42 <tanpau42@gmail.com>
2026-02-01 01:13:18 +0100

    chore: clean and rename files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,7 @@
-import { locationsRouter } from './routers/locations-router';
-import { sessionRouter } from './routers/session';
+import { sessionRouter } from './routers/session-router';
 import { createTRPCRouter } from './trpc';
 
 export const appRouter = createTRPCRouter({
-    locations: locationsRouter,
     session: sessionRouter,
 });
 
